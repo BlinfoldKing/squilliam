9985a29c2e1ed793c4ca4936099dc4c703d07e2f
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-28 11:30:30 +0700

    add menu

--- main.rb
+++ main.rb
@@ -1,1 +1,30 @@
-puts "Hello World!"
+module Squilliam
+  def self.read_input
+    return gets.chomp
+  end
+
+  def self.run!
+    loop do
+      puts "1. Tampilkan BFR dan Fanout Ratio Setiap Tabel"
+      puts "2. Tampilkan Total Blok Data + Blok Index Setiap Tabel"
+      puts "3. Tampilkan Jumlah Blok yang diakses untuk pencarian Record"
+      puts "4. Tampilkan QEP dan Cost"
+      puts "5. Tampilkan Isi File Shared Pool"
+      print "=>"
+      input = read_input
+      break unless input != "6"
+      puts ""
+
+      case input
+      when 1
+        puts "hello world"
+      when 2..5
+        puts "hello world"
+      else
+        puts "Error: Invalid Input"
+      end
+    end
+  end
+end
+
+Squilliam::run!
