c08fab0104bc9acbff6bc0c0039dda13e98ad7d5
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-28 23:23:28 +0700

    Add implementation to calculate BFR and Fan Out Ratio

--- lib/number1.rb
+++ lib/number1.rb
@@ -2,8 +2,13 @@ require_relative 'helper';
 
 module Squilliam
   class Number1
-    def self.run!
-      # write implementation here
+    def self.run(p, b, dictionary)
+      dictionary.each do |item|
+        bfr = (b/item["r"]).floor
+        fanout = (b/(item["v"]+p)).floor
+        puts 'BFR ' + item["title"] + ' : ' + bfr.to_s
+        puts 'Fan Out Ratio ' + item["title"] + ' : ' + fanout.to_s
+      end
     end
   end
 end
