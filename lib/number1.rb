a0c836e2ed53cdb25e17e3f3bd4e73bdc28c98a0
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-28 23:26:51 +0700

    Change parameter name

--- lib/number1.rb
+++ lib/number1.rb
@@ -2,8 +2,8 @@ require_relative 'helper';
 
 module Squilliam
   class Number1
-    def self.run(p, b, dictionary)
-      dictionary.each do |item|
+    def self.run(p, b, tables)
+      tables.each do |item|
         bfr = (b/item["r"]).floor
         fanout = (b/(item["v"]+p)).floor
         puts 'BFR ' + item["title"] + ' : ' + bfr.to_s
