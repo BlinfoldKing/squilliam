6049dc9ad33cba626d07a7a80b9d5d5df1f98f53
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-28 23:47:27 +0700

    Add implemetation to calculate table and index size

--- lib/number2.rb
+++ lib/number2.rb
@@ -2,8 +2,15 @@ require_relative 'helper';
 
 module Squilliam
   class Number2
-    def self.run!
-      # write implementation here
+    def self.run(p, b, tables)
+      tables.each do |item|
+        bfr = (b/item["r"]).floor
+        fanout = (b/(item["v"]+p)).floor
+        table = (item["n"]/bfr).ceil
+        index = (item["n"]/fanout).ceil
+        puts 'Tabel Data ' + item["title"] + ' : ' + table.to_s + ' blok'
+        puts 'Indeks ' + item["title"] + " : " + index.to_s + " blok"
+      end
     end
   end
 end
