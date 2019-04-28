478f4a2aef568a0aecb6536d903220b1298e8a94
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-29 01:13:51 +0700

    Add implementation to calculate record search

--- lib/number3.rb
+++ lib/number3.rb
@@ -2,8 +2,23 @@ require_relative 'helper';
 
 module Squilliam
   class Number3
-    def self.run!
-      # write implementation here
+    def self.run(p, b, tables, recordSearch, tableSearch)
+      res = 0
+      
+      tables.each do |item|
+          if item["title"] === tableSearch
+              break
+          end
+          res+=1
+      end
+      
+      bfr = (b/Float(tables[res]["r"])).floor
+      fanout = (b/Float(tables[res]["v"]+p)).floor
+      withIndex = (recordSearch/fanout).ceil
+      withoutIndex = (recordSearch/bfr).ceil
+
+      puts 'Menggunakan indeks, jumlah blok yang diakses : ' + withIndex.to_s + ' blok'
+      puts 'Tanpa indeks, jumlah blok yang diakses : ' + withIndex.to_s + ' blok' 
     end
   end
 end
